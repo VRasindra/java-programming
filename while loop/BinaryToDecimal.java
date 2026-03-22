589d5a62a9eb7774739506505b4a5b289b3b4c94
VRasindra <vrasindra@gmail.com>
2026-03-22 18:34:49 +0530

    Add files via upload

---
+++ while loop/BinaryToDecimal.java
@@ -0,0 +1,18 @@
+//16 Binary to Decimal Converter
+// import java.util.Scanner;
+// public class BinaryToDecimal{
+// public static void main(String[] args){
+//   Scanner sc = new Scanner(System.in);
+//     int binary=sc.nextInt();
+//     int number = binary,decimal =0,power=0;
+//     while (binary > 0){
+//           int digit =binary % 10;
+//           decimal += digit *Math.pow(2,power);
+//           power++;
+//           binary /= 10; 
+//     }
+//     System.out.println("Binary:"+ number);
+//     System.out.println("Decimal:" + decimal);
+// }
+// }
+
