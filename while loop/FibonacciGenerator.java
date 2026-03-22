ba20ab0327fbb6a17290c84c0c766573ff03ff99
VRasindra <vrasindra@gmail.com>
2026-03-22 18:36:36 +0530

    Add files via upload

---
+++ while loop/FibonacciGenerator.java
@@ -0,0 +1,17 @@
+//10. Fibonacci Sequence Generator
+// import java.util.Scanner;
+// public class FibonacciGenerator {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int a = 0, b = 1, count = 1;
+//         System.out.println("Fibonacci sequence (" + n + " terms):");
+//         while (count <= n) {
+//             System.out.print(a + " ");
+//             int next = a + b;
+//             a = b;
+//             b = next;
+//             count++;
+//         }
+//     }
+// }
