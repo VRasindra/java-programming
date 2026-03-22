d108ccea01cef1aa2f1b8d575b636df7846b8c86
VRasindra <vrasindra@gmail.com>
2026-03-22 18:35:31 +0530

    Add files via upload

---
+++ while loop/DecimalToBinary.java
@@ -0,0 +1,17 @@
+//19 Decimal to Binary Converter
+// import java.util.Scanner;
+// public class DecimalToBinary {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int decimal = sc.nextInt();
+//         int original = decimal;
+//         String binary = "";
+
+//         while (decimal > 0) {
+//             binary = (decimal % 2) + binary;
+//             decimal /= 2;
+//         }
+//         System.out.println("Decimal: " + original);
+//         System.out.println("Binary: " + binary);
+//     }
+// }
