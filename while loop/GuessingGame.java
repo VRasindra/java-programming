d2f884ce7fbca5a572aa02941bc359f74f26faef
VRasindra <vrasindra@gmail.com>
2026-03-23 19:49:33 +0530

    Add files via upload

---
+++ while loop/GuessingGame.java
@@ -0,0 +1,24 @@
+//3. Number Guessing Game
+// import java.util.Scanner;
+// public class GuessingGame {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int secretNumber = sc.nextInt();
+//         int attempts = 0;
+//         int guess;
+//         do {
+//             System.out.print("Guess the number (1-100): ");
+//             guess = sc.nextInt();
+//             System.out.println(guess);
+//             attempts++;
+//             if (guess > secretNumber) {
+//                 System.out.println("Too high!");
+//             } else if (guess < secretNumber) {
+//                 System.out.println("Too low!");
+//             }
+//         } while (guess != secretNumber);
+
+//         System.out.println("Correct! Attempts: " + attempts);
+//     }
+// }
+
