28c781315305947f9e9b91dac313a7683b1c98d3
VRasindra <vrasindra@gmail.com>
2026-03-23 19:50:00 +0530

    Add files via upload

---
+++ while loop/HCFSubtraction.java
@@ -0,0 +1,14 @@
+//24 HCF using Subtraction method
+// import java.util.Scanner;
+// public class HCFSubtraction {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         while (a != b) {
+//             if (a > b) a = a - b;
+//             else b = b - a;
+//         }
+//         System.out.println("HCF of " + sc.nextInt() + " and " + sc.nextInt() + " = " + a);
+//     }
+// }
