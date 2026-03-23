ec6eca121889126e6908a712663d6c5d5e328ed3
VRasindra <vrasindra@gmail.com>
2026-03-23 19:51:13 +0530

    Add files via upload

---
+++ while loop/LCMCalculator.java
@@ -0,0 +1,17 @@
+//18 LCM Calculator
+// import java.util.Scanner;
+// public class LCMCalculator {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int gcdA = a, gcdB = b;
+//         while (gcdB != 0) {
+//             int temp = gcdB;
+//             gcdB = gcdA % gcdB;
+//             gcdA = temp;
+//         }
+//         int lcm = (a * b) / gcdA;
+//         System.out.println("LCM of " + a + " and " + b + " = " + lcm);
+//     }
+// }
