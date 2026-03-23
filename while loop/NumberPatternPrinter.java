80e5c7784b6099ac3be95062a96364c33c99f0ac
VRasindra <vrasindra@gmail.com>
2026-03-23 19:53:04 +0530

    Add files via upload

---
+++ while loop/NumberPatternPrinter.java
@@ -0,0 +1,19 @@
+//25 Number pattern printer
+// import java.util.Scanner;
+// public class NumberPatternPrinter {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int row = 1;
+//         System.out.println("Pattern:");
+//         do {
+//             int col = 1;
+//             do {
+//                 System.out.print(row + " ");
+//                 col++;
+//             } while (col <= row);
+//             System.out.println();
+//             row++;
+//         } while (row <= n);
+//     }
+// }
