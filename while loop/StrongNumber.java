9add20427f233047bda4127fc95faaface770bda
VRasindra <vrasindra@gmail.com>
2026-03-24 09:12:56 +0530

    Add files via upload

---
+++ while loop/StrongNumber.java
@@ -0,0 +1,30 @@
+//20 Strong number Checker
+// import java.util.Scanner;
+// public class StrongNumber {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int num = sc.nextInt();
+//         int original = num, sum = 0;
+
+//         while (num > 0) {
+//             int digit = num % 10;
+//             int fact = 1;
+//             int i = 1;
+//             while (i <= digit) {
+//                 fact *= i;
+//                 i++;
+//             }
+//             sum += fact;
+//             num /= 10;
+//         }
+
+//         if (sum == original)
+//             System.out.println(original + " is a strong number");
+//         else
+//             System.out.println(original + " is not a strong number");
+//     }
+// }
+
+
+
+
