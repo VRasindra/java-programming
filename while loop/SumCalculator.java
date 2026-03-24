aa4b175affbaf205820c868275c8edcca1d310b1
VRasindra <vrasindra@gmail.com>
2026-03-24 09:13:52 +0530

    Add files via upload

---
+++ while loop/SumCalculator.java
@@ -0,0 +1,21 @@
+//2. sum calculator until Zero
+// import java.util.Scanner;
+// public class SumCalculator {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int sum = 0, count = 0;
+//         int num;
+//         while (true) {
+//             System.out.print("Enter number (0 to stop): ");
+//             num = sc.nextInt();
+//             System.out.println(num);
+//             if (num == 0) break;
+//             sum += num;
+//             count++;
+//         }
+//         System.out.println("Total sum: " + sum);
+//         System.out.println("Count: " + count);
+        
+//     }
+
+//     }
