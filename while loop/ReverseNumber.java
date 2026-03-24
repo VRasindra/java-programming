2ca9db39d74197a231f537889cdb9d83ec1c6d8c
VRasindra <vrasindra@gmail.com>
2026-03-24 09:12:28 +0530

    Add files via upload

---
+++ while loop/ReverseNumber.java
@@ -0,0 +1,18 @@
+//5. Reverse Number
+// import java.util.Scanner;
+// public class ReverseNumber {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int number = sc.nextInt();
+//         int reversed = 0;
+//         int original = number;
+
+//         while (number > 0) {
+//             int digit = number % 10;
+//             reversed = reversed * 10 + digit;
+//             number /= 10;
+//         }
+//         System.out.println("Original: " + original);
+//         System.out.println("Reversed: " + reversed);
+//     }
+// }
