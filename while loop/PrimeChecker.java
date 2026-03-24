c9287de654982dc6b9fc4ba6a2e823011272622f
VRasindra <vrasindra@gmail.com>
2026-03-24 09:11:52 +0530

    Add files via upload

---
+++ while loop/PrimeChecker.java
@@ -0,0 +1,22 @@
+//9. prime Number Checker
+// import java.util.Scanner;
+// public class PrimeChecker {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         boolean isPrime = true;
+//         int i = 2;
+//         while (i * i <= n) {
+//             if (n % i == 0) {
+//                 isPrime = false;
+//                 break;
+//             }
+//             i++;
+//         }
+//         if (isPrime) {
+//             System.out.println(n + " is prime");
+//         } else {
+//             System.out.println(n + " is not prime");
+//         }
+//     }
+// }
