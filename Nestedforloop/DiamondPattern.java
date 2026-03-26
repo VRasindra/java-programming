aa4c2486a472398f553fb059f9d8f84b669aae94
VRasindra <vrasindra@gmail.com>
2026-03-26 18:37:07 +0530

    Add files via upload

---
+++ Nestedforloop/DiamondPattern.java
@@ -0,0 +1,29 @@
+// 7: Diamond Pattern Generator
+// import java.util.Scanner;
+// public class DiamondPattern {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int size = sc.nextInt();  
+//         System.out.println("Diamond Pattern:");
+//         int middle = size / 2;
+//         for (int i = 0; i <= middle; i++) {
+//             // for (int j = 0; j < middle - i; j++) {
+//             //     System.out.print(" ");
+//             // }
+//             for (int j = 0; j < (2 * i + 1); j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//        }        
+//         for (int i = middle - 1; i >= 0; i--) {            
+//             // for (int j = 0; j < middle - i; j++) {
+//             //     System.out.print(" ");
+//             // }
+//             for (int j = 0; j < (2 * i + 1); j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
