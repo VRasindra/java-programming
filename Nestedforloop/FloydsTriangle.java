3a69140db00e752cb6e464e53cdc39d7cfc67f67
VRasindra <vrasindra@gmail.com>
2026-03-26 18:38:01 +0530

    Add files via upload

---
+++ Nestedforloop/FloydsTriangle.java
@@ -0,0 +1,18 @@
+//19: Floyd's Triangle Generator
+// import java.util.Scanner;
+// public class FloydsTriangle {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int rows = sc.nextInt();
+//         System.out.println("Floyd's Triangle:");
+//         int num = 1; 
+//         for (int i = 1; i <= rows; i++) {
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print(num + " ");
+//                 num++; 
+//             }
+//             System.out.println(); 
+//         }
+//         sc.close();
+//     }
+// }
