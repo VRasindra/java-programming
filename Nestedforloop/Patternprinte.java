46bb3439d81be12f1c30f520590ded4974bbac43
VRasindra <vrasindra@gmail.com>
2026-03-29 21:32:25 +0530

    Add files via upload

---
+++ Nestedforloop/Patternprinte.java
@@ -0,0 +1,20 @@
+// 3: Pattern Printer – Right Triangle
+// import java.util.Scanner;
+// public class Patternprinte{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int height = sc.nextInt();
+//         String character = sc.next();
+//         System.out.println("Right Triangle Pattern:");
+//         for (int i = 1; i <= height; i++) {
+//             for (int j = 1; j <= i; j++) {
+//                 if (character.equalsIgnoreCase("star")) {
+//                     System.out.print("*");
+//                 } else if (character.equalsIgnoreCase("number")) {
+//                     System.out.print(j);
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
