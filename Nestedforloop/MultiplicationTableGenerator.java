7e64959ebbcde15d873c44c34317f43bff6cbe28
VRasindra <vrasindra@gmail.com>
2026-03-29 21:28:13 +0530

    Add files via upload

---
+++ Nestedforloop/MultiplicationTableGenerator.java
@@ -0,0 +1,21 @@
+//1 Multiplication Table Generator
+// import java.util.Scanner;
+// public class MultiplicationTableGenerator {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int rows = sc.nextInt();
+//         int cols = sc.nextInt();
+//         System.out.println("Multiplication Table (" + rows + " x " + cols + "):");
+//         for (int j = 1; j <= cols; j++) {
+//             System.out.printf("%d", j);
+//         }
+//         System.out.println();
+//         for (int i = 1; i <= rows; i++) {
+//             System.out.printf("%d", i);
+//             for (int j = 1; j <= cols; j++) {
+//                 System.out.printf("%d", i*j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
