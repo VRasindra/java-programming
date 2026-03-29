c18c574de9940c33344175adefadcadd49992ab6
VRasindra <vrasindra@gmail.com>
2026-03-29 21:32:55 +0530

    Add files via upload

---
+++ Nestedforloop/SeatingArrangementMatrix.java
@@ -0,0 +1,19 @@
+//2: Seating Arrangement Matrix
+// import java.util.Scanner;
+// public class SeatingArrangementMatrix {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int rows = sc.nextInt();
+//         int seats = sc.nextInt();
+//         int totalSeats = rows * seats;
+//         System.out.println("Seating Arrangement:");
+//         for (int i=1;i<=rows;i++){
+//             System.out.print("Row "+i+": ");
+//             for (int j=1;j<=seats;j++){
+//                 System.out.print("Seat-"+j+" ");
+//             }
+//             System.out.println();
+//         }
+//         System.out.println("Total Seats: " +totalSeats);
+//     }
+// }
