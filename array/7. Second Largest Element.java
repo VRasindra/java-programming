9d30c95599ad85cd5cd7b389ec7df0ad7eb5ae5e
Rasindra Venkatachalam <vrasindra@gmail.com>
2026-05-28 10:05:43 +0530

    Add files via upload

---
+++ array/7. Second Largest Element.java
@@ -0,0 +1,24 @@
+ class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {10,20,4,45,99};
+
+        int largest = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for(int i=0; i<arr.length; i++) {
+
+            if(arr[i] > largest) {
+
+                second = largest;
+                largest = arr[i];
+
+            } else if(arr[i] > second && arr[i] != largest) {
+
+                second = arr[i];
+            }
+        }
+
+        System.out.println("Second Largest = " + second);
+    }
+}
