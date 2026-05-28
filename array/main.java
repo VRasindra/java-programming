ae582f7909bbed39bf2a1cc0ee60d80f829d8827
Rasindra Venkatachalam <vrasindra@gmail.com>
2026-05-28 10:02:20 +0530

    Add files via upload

---
+++ array/main.java
@@ -0,0 +1,11 @@
+//1. Print Array Elements
+public class main {
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5};
+
+        for(int i=0; i<arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
