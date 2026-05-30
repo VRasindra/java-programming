49cd226c1d1125a79cc2645776c498c63aa7fbf6
Rasindra Venkatachalam <vrasindra@gmail.com>
2026-05-30 09:41:06 +0530

    Add files via upload

---
+++ array/13.Merge Two Arrays.java
@@ -0,0 +1,20 @@
+ class Main {
+    public static void main(String[] args) {
+
+        int[] a = {1,2,3};
+        int[] b = {4,5,6};
+
+        int[] c = new int[a.length + b.length];
+
+        int k = 0;
+
+        for(int i : a)
+            c[k++] = i;
+
+        for(int i : b)
+            c[k++] = i;
+
+        for(int num : c)
+            System.out.print(num + " ");
+    }
+}
