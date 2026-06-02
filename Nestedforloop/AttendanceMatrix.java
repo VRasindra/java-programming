a3c1e56e484615cc8d3032a66a17d4e1a51e8b62
Rasindra Venkatachalam <vrasindra@gmail.com>
2026-06-02 23:20:15 +0530

    Update AttendanceMatrix.java

--- Nestedforloop/AttendanceMatrix.java
+++ Nestedforloop/AttendanceMatrix.java
@@ -1,4 +1,4 @@
-jjj // 8: Student Attendance Matrix
+jj // 8: Student Attendance Matrix
 // import java.util.Scanner;
 // public class AttendanceMatrix {
 //     public static void main(String[] args) {
